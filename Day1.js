7c05b58b20c82e5442b9ba2321b7c98c2ec5b38d
Sonia2105 <soniakakas897@gmail.com>
2024-07-19 16:45:01 +0530

    added day1.js file

---
+++ Day1.js
@@ -0,0 +1,37 @@
+// Activity Task-1
+var myBirthday = 21;
+console.log(myBirthday);
+
+// Task-2
+let myName = "Hello,I am Sonia";
+console.log(myName);
+
+// Task-3
+const isIssueSolved = true;
+console.log(isIssueSolved);
+
+// Task-4
+let myObject ={name:"Sonia", branch:"ECE"};
+let myClassmates=["bhumika","Aarti","Harshita","Ishita"]
+let myBirthday1 =21;
+let thisHappen = false;
+
+console.log(typeof myBirthday1);
+console.log(typeof myName);
+console.log(typeof myObject);
+console.log(typeof myClassmates);
+console.log(typeof thisHappen);
+
+// Task-5
+let myFavouriteNumber = 21;
+console.log("Initial Value:", myFavouriteNumber);
+
+myFavouriteNumber = 12;
+console.log("New Value:",myFavouriteNumber);
+
+// Task-6
+const myFavouriteNumber1 = 21;
+console.log("Initial Value:", myFavouriteNumber);
+
+myFavouriteNumber1 = 12;
+console.log("New Value:",myFavouriteNumber);
