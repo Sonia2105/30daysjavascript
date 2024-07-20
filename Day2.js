00e4f89af76c702737f6a832da19a035c080e758
Sonia2105 <soniakakas897@gmail.com>
2024-07-20 12:58:25 +0530

    day2.js file is added

---
+++ Day2.js
@@ -0,0 +1,75 @@
+// Task 1
+let a=6;
+let b=2;
+console.log(a+b); 
+
+// Task 2
+
+console.log(a-b); 
+
+//Task 3
+
+console.log(a*b); 
+
+//Task 4
+
+console.log(a/b); 
+
+//Task 5
+
+console.log(a%b); 
+
+//Task 6
+let x=9;
+x+=10;
+console.log(x); // 19
+
+//Task 7
+let y=34;
+y-=12;
+console.log(y); // 22
+
+//Task 8
+console.log(36>12); // true
+console.log(37<12); // false
+
+//Task 9
+console.log(21>=21); // true
+console.log(17<=12); // false
+
+//Task 10
+let x1=12;
+let x2="12";
+console.log(x1==x2);  // true
+console.log(x1===x2); // false
+
+//Task 11
+let i=56;
+while(i>0 && i%2==0)
+{
+    i=i/2;
+}
+console.log(i); // 7
+
+//Task 12
+let age=20;
+if(age==18 || age>18)
+{
+    console.log("eligible to vote") // this will be printed
+}
+else
+{
+    console.log("not eligible to vote");
+}
+
+//Task 13
+if(!false)
+{
+    console.log("stmt executed");
+}
+
+//Task 14
+let num=-7;
+console.log(num>=0?"positive":"negative");
+
+
