ba8035f2bb379992e89a3968295d89f0b2b95e14
Sonia2105 <soniakakas897@gmail.com>
2024-07-20 16:23:44 +0530

    output mentioned

--- Day2.js
+++ Day2.js
@@ -1,6 +1,8 @@
+ // Activity 1 :Arithmetic Operators
+
 // Task 1
-let a=6;
-let b=2;
+let a = 6;
+let b = 2;
 console.log(a+b); // output : 8
 
 // Task 2
@@ -19,6 +21,8 @@ console.log(a/b); // output : 3
 
 console.log(a%b); // output : 0
 
+// Activity 2
+
 //Task 6
 let x=9;
 x+=10;
@@ -28,6 +32,8 @@ console.log(x); // 19
 let y=34;
 y-=12;
 console.log(y); // 22
+
+// Activity 3 - comparison operators
 
 //Task 8
 console.log(36>12); // true
@@ -42,6 +48,8 @@ let x1=12;
 let x2="12";
 console.log(x1==x2);  // true
 console.log(x1===x2); // false
+
+// activity 4 - logical operators
 
 //Task 11
 let i=56;
@@ -68,8 +76,38 @@ if(!false)
     console.log("stmt executed"); // output : stmt executed
 }
 
+// activity 5 - ternary operators
+
 //Task 14
 let num=-7;
 console.log(num>=0?"positive":"negative"); // output : negative
 
+// FEATURE REQUEST
+// task -1 Arithmetic operator related
 
+let c = 21;
+
+console.log(c+2); // output:23
+console.log(c-3); // output:18
+console.log(c*2); // output:42
+console.log(c/7); // output:3
+console.log(c%3); // output:0
+console.log(c++); // output:21
+console.log(c--); // output:22
+console.log(c**2); // output:441
+
+// task -2 Comparsion and logical operator related
+
+console.log(5==5); // output : true 
+console.log(5!=5); // output : false
+console.log(5==='5'); // output : false
+console.log(3>5); // output : false
+console.log(3<9); // output :true
+
+console.log(3>5 && 4<9); // output : false
+console.log(3>5 || 9>4); // output : true
+console.log(!(a==4)); // output : true
+
+// task -3 ternary operator related
+
+c>18?console.log("greater") : console.log("smaller"); // output : greater 
